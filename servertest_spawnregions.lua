b0cc64da30891f13fc75bb47063fc586e839bdcb
Bill <contero@gmail.com>
2024-06-10 11:03:12 -0700

    Current PZ server config

---
+++ servertest_spawnregions.lua
@@ -0,0 +1,14 @@
+function SpawnRegions()
+	return {
+		{ name = "Bedford Falls", file = "media/maps/BedfordFalls/spawnpoints.lua" }, -- Didn't work?
+		--{ name = "Blackwood", file = "media/maps/Blackwood/spawnpoints.lua" },
+		{ name = "Fort Benning", file = "media/maps/Fort Benning/spawnpoints.lua" },
+		--{ name = "Louisville, KY", file = "media/maps/Louisville, KY/spawnpoints.lua" },
+		--{ name = "McCoysBunker", file = "media/maps/McCoysBunker/spawnpoints.lua" },
+		{ name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
+		{ name = "Riverside, KY", file = "media/maps/Riverside, KY/spawnpoints.lua" },
+		--{ name = "Rosewood, KY", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
+		{ name = "Seaside", file = "media/maps/Seaside/spawnpoints.lua" },
+		{ name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
+	}
+end
